bd32bfb3f98273f39798e11c688976ea8dca0428
Raffaele Meo <raffaelemeo77@gmail.com>
2025-03-15 10:14:29 +0100

    This is and algorithm to merge two sorted arrays into a single sorted array

---
+++ Project/mergeArray.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int array1 [4];
+    int array2 [4];
+    int merge [8];
+
+    for (int i = 0; i < 4; i++) {
+        cout << "Inserisci i numeri in ordine crescente (array 1): " <<  endl;
+        cin  >> array1[i];
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        cout << "Inserisci i numeri in ordine crescente (array 2): " <<  endl;
+        cin  >> array2[i];
+    }
+
+    int k = 0;
+    int i = 0;
+    int j = 0;
+
+    for (k = 0; k < 8; k++) {
+        cout << "Valore i: " << i << " Valore array: " << array1[i] << " Valore j: " << j << " Valore array: " << array2[j] << " Valore k: " << k << endl;
+        if ((i < 4) && (j < 4)) {
+            cout << "Caso di i e j < massimo i: " << i << " j: " << j << endl;
+            if(array1[i] < array2[j]) {
+                merge[k] = array1[i];
+                i++;
+            } else {
+                merge[k] = array2[j];
+                j++;
+            }
+        }  else if (i <  4) {
+            cout << "Caso di i minore del massimo i: " << i << endl;
+            merge[k] = array1[i];
+            i++;
+        } else if (j < 4) {
+            cout << "Caso di j minore del massimo j: " << j << endl;
+            merge[k] = array2[j];
+            j++;
+        }
+    }
+
+    cout << "Array dopo il merge:" << endl;
+    for(int i = 0; i < 8; i++) { 
+        cout << merge[i] << endl;
+    }
+    cout << endl;
+
+return 0;
+}
