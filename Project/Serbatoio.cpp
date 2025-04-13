2b3c4095ba60866c87c4141a9183d1633647c8c4
Raffaele Meo <raffaelemeo77@gmail.com>
2025-04-13 16:56:06 +0200

    Now the code can print a sciplot graph rapresenting the water flow

--- Project/Serbatoio.cpp
+++ Project/Serbatoio.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <cmath>
+#include <vector> 
+#include <sciplot/sciplot.hpp>
 
+using namespace sciplot;
 using namespace std;
 
 void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, int* interval, int* tempoSimu) {
@@ -23,14 +26,21 @@ void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, 
     cin >> *tempoSimu;
 }
 
-void PortataOut (float altezza, float* livello, float areaS, float areaV, int interval, int tempSimu, float* acquaOut) {
+void PortataOut (float altezza, float* livello, float areaS, float areaV, int interval, int tempSimu, float* acquaOut, vector<float> &tempi, vector<float> &livelli) {
 
     for (int i = 0; i < tempSimu; i += interval) {
         float portata = areaV * sqrt(2 * 9.81 * (*livello));
         float rilascio = portata * interval;
-        *livello = *livello - (rilascio / areaS);
+	float variazioneLivello = rilascio / areaS;
+	if (*livello > variazioneLivello) {
+        	*livello = *livello - variazioneLivello;
+	} else {
+		*livello = 0.0;
+	}
         *acquaOut += rilascio;
         cout << "Il livello del serbatoio nel secondo " << i << " e di: " << *livello << endl;
+        tempi.push_back(i);
+        livelli.push_back(*livello);
     }
 }
 
@@ -44,12 +54,40 @@ int main() {
     int tempoSimulazione;
     float acquaOut = 0;
 
+    // Create a vector with values from 0 to pi divived into 200 uniform intervals for the x-axis
+    vector<float> tempi;
+    vector<float> livelli;
+
     RichiestaDati (&altezzaS, &livelloLiquid, &areaS, &areaV, &intervalloTemp, &tempoSimulazione);
     
-    PortataOut (altezzaS, &livelloLiquid, areaS, areaV, intervalloTemp, tempoSimulazione, &acquaOut);
+    PortataOut (altezzaS, &livelloLiquid, areaS, areaV, intervalloTemp, tempoSimulazione, &acquaOut, tempi, livelli);
 
     cout << "Quantità di acqua uscita: " << acquaOut << " m³" << endl;
     cout << "Livello finale del serbatoio: " << livelloLiquid << " m" << endl;
     
+    // Create a Plot object
+    Plot2D plot;
+
+    // Set the x and y labels
+    plot.xlabel("tempo");
+    plot.ylabel("livello");
+
+    // Set the legend to be on the bottom along the horizontal
+    plot.legend()
+        .atOutsideBottom()
+        .displayHorizontal()
+        .displayExpandWidthBy(2);
+
+    // Plot sin(i*x) from i = 1 to i = 6
+    plot.drawCurve(tempi, livelli);
+
+    // Create figure to hold plot
+    Figure fig = {{plot}};
+    // Create canvas to hold figure
+    Canvas canvas = {{fig}};
+
+    // Show the plot in a pop-up window
+    canvas.show();
+
     return 0;
 }
