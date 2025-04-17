e647c35aa39e7128b849c1a73b041251c862c2b7
Raffaele Meo <raffaelemeo77@gmail.com>
2025-04-17 19:38:19 +0200

    I've added a valve at the top of the tank and when the tank is clear and if there is still time it starts to pour liquid (PERFECT LIQUID) inside the tank and if it reaches full and there is still time it starts to let the liquid flow out again

--- Project/Serbatoio.cpp
+++ Project/Serbatoio.cpp
@@ -6,7 +6,7 @@
 using namespace sciplot;
 using namespace std;
 
-void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, int* interval, int* tempoSimu) {
+void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, int* interval, int* tempoSimu, float* areaValvolaInp) {
     cout << "Inserisci l'atltezza del serbatoio: " << endl;
     cin >> *altezza;
 
@@ -19,6 +19,9 @@ void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, 
     cout << "Inserisci l'area della valvola: " << endl;
     cin >> *areaV;
 
+    cout << "Inserisci portata della valola in input: " << endl;
+    cin >> *areaValvolaInp;
+
     cout << "Inserisci l'intervallo di tempo (in secondi): " << endl;
     cin >> *interval;
 
@@ -26,18 +29,41 @@ void RichiestaDati (float* altezza, float* livello, float* areaS, float* areaV, 
     cin >> *tempoSimu;
 }
 
-void PortataOut (float altezza, float* livello, float areaS, float areaV, int interval, int tempSimu, float* acquaOut, vector<float> &tempi, vector<float> &livelli) {
+void PortataOut (float altezza, float* livello, float areaS, float areaV, int interval, int tempSimu, float* acquaOut, vector<float> &tempi, vector<float> &livelli, float portataInput) {
+
+    bool scarico = true;
 
     for (int i = 0; i < tempSimu; i += interval) {
+
+      if (scarico) {
         float portata = areaV * sqrt(2 * 9.81 * (*livello));
         float rilascio = portata * interval;
-	float variazioneLivello = rilascio / areaS;
-	if (*livello > variazioneLivello) {
-        	*livello = *livello - variazioneLivello;
-	} else {
-		*livello = 0.0;
-	}
-        *acquaOut += rilascio;
+	      float variazioneLivello = rilascio / areaS;
+        
+	      if (*livello > variazioneLivello) {
+          *livello = *livello - variazioneLivello;
+          *acquaOut += rilascio;
+	      } else {
+
+		      *acquaOut += *livello * areaS;
+          *livello = 0.0;
+          scarico = false;
+
+        }
+
+      } else {
+
+        float Riempimento = portataInput * interval;
+        float variazioneLivello = Riempimento / areaS;
+        *livello += variazioneLivello;
+
+        if (*livello >= altezza) {
+            *livello = altezza;
+            scarico = true;
+            cout << "Il serbatoio e pieno al secondo " << i << endl;
+        }
+	    }
+
         cout << "Il livello del serbatoio nel secondo " << i << " e di: " << *livello << endl;
         tempi.push_back(i);
         livelli.push_back(*livello);
@@ -53,14 +79,14 @@ int main() {
     int intervalloTemp;
     int tempoSimulazione;
     float acquaOut = 0;
-
+    float areaValvolaInp;
     // Create a vector with values from 0 to pi divived into 200 uniform intervals for the x-axis
     vector<float> tempi;
     vector<float> livelli;
 
-    RichiestaDati (&altezzaS, &livelloLiquid, &areaS, &areaV, &intervalloTemp, &tempoSimulazione);
+    RichiestaDati (&altezzaS, &livelloLiquid, &areaS, &areaV, &intervalloTemp, &tempoSimulazione, &areaValvolaInp);
     
-    PortataOut (altezzaS, &livelloLiquid, areaS, areaV, intervalloTemp, tempoSimulazione, &acquaOut, tempi, livelli);
+    PortataOut (altezzaS, &livelloLiquid, areaS, areaV, intervalloTemp, tempoSimulazione, &acquaOut, tempi, livelli, areaValvolaInp);
 
     cout << "Quantità di acqua uscita: " << acquaOut << " m³" << endl;
     cout << "Livello finale del serbatoio: " << livelloLiquid << " m" << endl;
