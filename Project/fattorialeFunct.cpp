46bad542e8be2910c1f1f91f8ef6c1f68af8af37
Raffaele Meo <raffaelemeo77@gmail.com>
2025-04-17 19:41:35 +0200

    I calculates the factorial ! of a number using a recursive function

---
+++ Project/fattorialeFunct.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+unsigned long long fact(unsigned long long n){
+  if (n == 1)
+  {
+    return 1;
+  }
+  return n * fact(n - 1);
+}
+
+int main()
+{
+	 std::cout << "Hello" << std::endl;
+   std::cout << "Il fattoriale di 10: " << fact(10) << std::endl;
+}
